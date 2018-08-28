52855bb2d7a6d152ad720c39eb712c49130eb122
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-28 22:38:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,12 @@
 require "pry"
 
-def nyc_pigeon_organizer(data)
   # need to collect each pigeon by name 
   # not have any duplicates
   # have color, gender, lives as the keys (1 level down)
-end
 
 
 
+def nyc_pigeon_organizer(data)
 data.each do |first_level, all_other|
   all_other.each do |category, array|
     binding.pry
@@ -16,3 +15,4 @@ data.each do |first_level, all_other|
       end 
     end 
   end 
+end 
