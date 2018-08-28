b37e2bcbdc4845c8fd2bc779b16b266a192d6432
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-28 22:37:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 def nyc_pigeon_organizer(data)
   # need to collect each pigeon by name 
   # not have any duplicates
-  # have color, 
+  # have color, gender, lives as the keys (1 level down)
 end
